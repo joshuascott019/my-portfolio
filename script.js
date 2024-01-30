38439f6c38ad3987468a5ca5a9fc8fadbc8f0c04
Joshua Scott <joshua.ds.s019@gmail.com>
2024-01-30 12:31:11 -0800

    save point

--- script.js
+++ script.js
@@ -6,17 +6,17 @@ const navMenu = document.getElementById('menu-items')
 menuBtn.addEventListener('click', () => {
     menuBtn.classList.add('hidden')
     closeBtn.classList.remove('hidden')
-    navMenu.classList.remove('hidden')
+    navMenu.style.display = 'flex'
 })
 closeBtn.addEventListener('click', () => {
     closeBtn.classList.add('hidden')
-    navMenu.classList.add('hidden')
+    navMenu.style.display = 'none'
     menuBtn.classList.remove('hidden')
 })
 menuItems.forEach((item) => {
     item.addEventListener('click', () => {
         closeBtn.classList.add('hidden')
-        navMenu.classList.add('hidden')
+        navMenu.style.display = 'none'
         menuBtn.classList.remove('hidden')
     })
 })
