6ccfb4df9a67eb9c6a63c6c39bf8a0519aa52710
Joshua Scott <joshua.ds.s019@gmail.com>
2025-12-17 11:32:15 -0300

    feat: add CardList mapping and component

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,10 +1,15 @@
 import Card from './Card.jsx';
 
-const CardList = () => {
+const CardList = ({ projects }) => {
   return (
-    <div>
-      <Card />
-    </div>
+    <ul className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+      {projects.map((project) => (
+        <li key={project.id}>
+          <Card project={project} />
+        </li>
+      ))}
+    </ul>
   );
 };
+
 export default CardList;
