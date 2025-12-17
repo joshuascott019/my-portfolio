caeec154317252533e8be1f6af854e51498c79aa
Joshua Scott <joshua.ds.s019@gmail.com>
2025-12-17 11:34:47 -0300

    feat: add PortfolioPage base component code

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -1,11 +1,16 @@
 import CardList from '../components/CardList.jsx';
+import projects from '../data/projects.json';
 
 const PortfolioPage = () => {
   return (
-    <div className="bg-white px-4 py-16">
-      Coming Soon...
-      <CardList />
-    </div>
+    <section className="bg-white px-4 py-16">
+      <div className="mx-auto max-w-6xl">
+        <h1 className="mb-8 text-3xl font-semibold">Portfolio</h1>
+
+        <CardList projects={projects} />
+      </div>
+    </section>
   );
 };
+
 export default PortfolioPage;
